d261394a88432e2099c1e968236b768581414a6e
araz0515 <arazkubtanjian@gmail.com>
2023-09-07 02:53:04 -0400

    API Integration

--- src/Dictionary.js
+++ src/Dictionary.js
@@ -1,11 +1,18 @@
 import React, { useState } from "react";
 import "./Dictionary.css";
+import axios from "axios";
 
 export default function Dictionary() {
   let [value, setValue] = useState(null);
+  function displayDefinition(response) {
+    console.log(response.data);
+  }
+
   function search(event) {
     event.preventDefault();
-    alert(`Searching for ${value} definition...`);
+    let apiKey = "ea34ad36f9tbf5ceb9037o7457b5a408";
+    let apiUrl = `https://api.shecodes.io/dictionary/v1/define?word=${value}&key=${apiKey}`;
+    axios.get(apiUrl).then(displayDefinition);
   }
 
   function handleUpdate(event) {
