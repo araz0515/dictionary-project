3934b41abba7f0f61fb8cb29094ed43591c687b7
araz0515 <arazkubtanjian@gmail.com>
2023-09-08 00:00:16 -0400

    Centered results section

--- src/Results.js
+++ src/Results.js
@@ -5,7 +5,7 @@ export default function Results(props) {
   console.log(props);
   if (props.data) {
     return (
-      <div className="Results">
+      <div className="Results text-center mt-4">
         <h2>{props.data.word}</h2>
         {props.data.meanings.map(function (meaning, index) {
           return (
