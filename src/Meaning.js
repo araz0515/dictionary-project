4cfc3b8667fa59aefac43000b0f71bcc829583aa
araz0515 <arazkubtanjian@gmail.com>
2023-09-09 01:53:58 -0400

    Changed examples & h3 styling

--- src/Meaning.js
+++ src/Meaning.js
@@ -1,12 +1,13 @@
 import React from "react";
 import Synonyms from "./Synonyms";
+import "./Meaning.css";
 
 export default function Meaning(props) {
   return (
-    <div className="Meaming>">
+    <div className="Meaning">
       <h3>{props.meaning.partOfSpeech}</h3>
       <p>{props.meaning.definition}</p>
-      <p>{props.meaning.example}</p>
+      <p className="example">{props.meaning.example}</p>
       <Synonyms synonyms={props.meaning.synonyms} />
     </div>
   );
